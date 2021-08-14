786cc90b525b7ad3b40aae4a245e2401dfa5e38b
wilkersoh <plgu@hotmail.com>
2021-08-14 11:28:18 +0800

    update: lesson-001

--- index.js
+++ index.js
@@ -42,36 +42,3 @@ console.log('i_am_string :>> ', i_am_string);
 if(i_am_string === "string") {
   console.log('i_am_string :>> ', i_am_string);
 }
-
-
-/**
- *
- *
- *  Structural Types
- *
- */
-
-/**
- * null & undefined
- */
-var i_am_null = null;
-console.log('i_am_null :>> ', i_am_null); // null
-
-var i_am_undefined;
-console.log('i_am_undefined :>> ', i_am_undefined); // undefined
-
-/**
- * Note:
- * Null 和 undefined 有什麼差別？
- * 1. 可以說是差不多的 但有些地方 還是會不同
- * 2. null 和 undefined 都是 return false
- * 3. 他們不一樣的地方在於 javascript裡的 object types， 它們是屬於不同的
- */
-if (i_am_null) {
-  console.log("if i_am_null :>> ", i_am_null);
-} else {
-  console.log("else i_am_null :>> ", i_am_null); // false
-}
-
-console.log('i_am_null typeof :>> ', typeof i_am_null); // object type
-console.log('i_am_undefined typeof :>> ', typeof i_am_undefined); // undefined
---
+++ structural.js
@@ -0,0 +1,36 @@
+/**
+ * Use terminal to run this script
+ * > $ node structural.js
+ */
+
+/**
+ *
+ *
+ *  Structural Types
+ *
+ */
+
+/**
+ * null & undefined
+ */
+var i_am_null = null;
+console.log("i_am_null :>> ", i_am_null); // null
+
+var i_am_undefined;
+console.log("i_am_undefined :>> ", i_am_undefined); // undefined
+
+/**
+ * Note:
+ * Null 和 undefined 有什麼差別？
+ * 1. 可以說是差不多的 但有些地方 還是會不同
+ * 2. null 和 undefined 都是 return false
+ * 3. 他們不一樣的地方在於 javascript裡的 object types， 它們是屬於不同的
+ */
+if (i_am_null) {
+  console.log("if i_am_null :>> ", i_am_null);
+} else {
+  console.log("else i_am_null :>> ", i_am_null); // false
+}
+
+console.log("i_am_null typeof :>> ", typeof i_am_null); // object type
+console.log("i_am_undefined typeof :>> ", typeof i_am_undefined); // undefined
