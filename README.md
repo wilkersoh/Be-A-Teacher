45949f259d877fcb31219a4a334c022f2479cbe4
wilkersoh <plgu@hotmail.com>
2021-08-14 11:12:54 +0800

    update: lesson 001 types

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Javascirpt Types
+  * [Data Types](#data-types)
+  * [Structural Types](#structural-types)
+
+# Data Types
+Javascipt裡面有6個 **data types**
+1. undefined
+2. Boolean
+3. Number
+4. String
+5. BigInt
+6. Symbol
+
+# Structural Types
+1. Object
+2. Function
+3. null
--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("types");
+/**
+ * Use terminal to run this script
+ * > $ node index.js
+ */
+
+/**
+ * undefined
+ */
+console.log("read_before_create :>> ", read_before_create) // undefined
+var read_before_create;
+
+/**
+ * boolean
+ */
+var i_am_boolean = true;
+console.log('i_am_boolean :>> ', i_am_boolean); // true
+
+/**
+ * number
+ */
+var i_am_number = 10;
+console.log('i_am_number :>> ', i_am_number); // 10
+
+/**
+ * string
+ */
+var i_am_string = "Who am i";
+console.log('i_am_string :>> ', i_am_string); // Who am i
+
+
+/**
+ * 我們怎樣 檢查 它是 屬於什麼 types呢？
+ * 用 typeof
+ */
+if(i_am_string === "string") {
+  console.log('i_am_string :>> ', i_am_string);
+}
+
